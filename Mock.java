96155788a852c87d7c37371c3aca73c84b13cdb0
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-09-19 14:10:41 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,7 +1,4 @@
-[
-  {"appId":"a1","lobId":"l1","name":"n1","value":"v1"},
-  {"appId":"a2","lobId":"l2","name":"n2","value":"v2"},
-  {"appId":"a3","lobId":"l3","name":"n3","value":"v3"},
-  {"appId":"a4","lobId":"l4","name":"n4","value":"v4"},
-  {"appId":"a5","lobId":"l5","name":"n5","value":"v5"}
-]
+INSERT INTO CONFIGURATION 
+    (ID, APP_ID, LOB_ID, PROP_NAME, PROP_VALUE)
+VALUES 
+    (2284, 'esignapi', 'dnaus', 'EXECUTE_PCOE_TEST', 'false');
