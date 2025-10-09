033a366cbc25200d05b2ef950a65921a8ea40a4c
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-10-09 14:28:34 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,17 +1,1 @@
-package com.td.esig.api.config;
-
-import java.lang.reflect.Field;
-
-final class TestUtils {
-    private TestUtils() {}
-
-    static void injectField(Object target, String fieldName, Object value) {
-        try {
-            Field f = target.getClass().getDeclaredField(fieldName);
-            f.setAccessible(true);
-            f.set(target, value);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to inject field: " + fieldName, e);
-        }
-    }
-}
+keytool -list -v -storetype PKCS12 -keystore aesig.notifier.dev.td.com.p12
