b2fb852ab7b0445bea6f9d0d2a6f50196057d04e
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-10-23 01:23:58 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,1 +1,10 @@
-yUO%%69&wjuK7h
+List<Long> times = new ArrayList<>();
+for (int i = 0; i < 100; i++) {
+    long start = System.nanoTime();
+    restTemplate.exchange(uri, method, requestEntity, responseType);
+    long end = System.nanoTime();
+    times.add((end - start) / 1_000_000);
+}
+
+double avg = times.stream().mapToLong(Long::longValue).average().orElse(0);
+System.out.println("Average ESL latency: " + avg + " ms");
