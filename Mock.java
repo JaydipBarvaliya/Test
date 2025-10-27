1621fa41f024cf9fa22fd4953e700dde4dabf63c
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-10-27 05:24:59 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,4 +1,2 @@
-keytool -importcert -keystore "C:\opt\springboot\security\cacerts" ^
--alias TRMIS ^
--file "C:\Users\taf5028\Documents\TD_Bank_Group_Internal_Browsing.cer" ^
--storepass changeit -noprompt
+-Djavax.net.ssl.trustStore="C:\Program Files\Java\jdk-17\lib\security\cacerts"
+-Djavax.net.ssl.trustStorePassword=changeit
