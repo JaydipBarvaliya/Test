006f3aa039af9b1bad02666d2b1f62eb1c8739a2
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-11-06 12:16:11 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,19 +1,3 @@
-@Test
-@TAE0141
-void testCheckMandatoryPropWhenNull() throws Exception {
-    String lobId = "dna";
-    String propToCheck = null;
-
-    // Mock config and isMandatory behaviour
-    when(config.getConfigProperty(lobId, propToCheck)).thenReturn(null);
-    CheckMandatoryProperty spyCheckMandatoryProperty = Mockito.spy(checkMandatoryProperty);
-    Mockito.doReturn(true).when(spyCheckMandatoryProperty).isMandatory(propToCheck);
-
-    // Expect SharedServiceLayerException to be thrown
-    SharedServiceLayerException ex = assertThrows(
-        SharedServiceLayerException.class,
-        () -> spyCheckMandatoryProperty.checkMandatoryProp(lobId, propToCheck)
-    );
-
-    assertEquals("Mandatory Config Property: null cannot be null", ex.getMessage());
-}
+ALTER TABLE CLIENT_APP_CONFIGURATION 
+RENAME CONSTRAINT "CONFIG UNIQUE CLIENT LOB" 
+TO CONFIG_UNIQUE_CLIENT_LOB;
