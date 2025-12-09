ea1648cf73e1ccdb551c4706143e6ceee16c9f1b
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-12-09 11:07:58 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,1 +1,12 @@
-2Nshv4tlCCjs5H@
+// Step 1: Detect duplicate IDs in incomingList
+List<String> incomingIds = incomingList.stream()
+        .map(UpdateAttachmentStatusRequest.AttachmentRequirementsInner::getAttachmentId)
+        .collect(Collectors.toList());
+
+Set<String> uniqueIds = new HashSet<>();
+
+for (String id : incomingIds) {
+    if (!uniqueIds.add(id)) {
+        throw commonUtil.buildBadRequestException("Duplicate attachment ID detected: " + id);
+    }
+}
