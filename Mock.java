87316101fd5698589efb18b8e8d0c0ceb2957de8
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-01-09 12:16:33 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,13 +1,4 @@
 if [[ "$DEPLOY_MODE" == "service" ]]; then
-  systemctl stop "$SERVICE_NAME"
-fi
-
-# always replace jar
-deploy_jar
-
-if [[ "$DEPLOY_MODE" == "service" ]]; then
-  systemctl start "$SERVICE_NAME"
-  run_health_check
-else
-  log "Batch job detected. Skipping service start and health check."
+  systemctl list-unit-files | grep -q "^${SERVICE_NAME}.service" \
+    || fail "Service not found: ${SERVICE_NAME}"
 fi
