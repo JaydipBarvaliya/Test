ef54b590fc59e00b4fff9d4ea26a7eda14654301
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-01-09 12:13:18 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,6 +1,13 @@
+if [[ "$DEPLOY_MODE" == "service" ]]; then
+  systemctl stop "$SERVICE_NAME"
+fi
+
+# always replace jar
+deploy_jar
+
+if [[ "$DEPLOY_MODE" == "service" ]]; then
+  systemctl start "$SERVICE_NAME"
+  run_health_check
 else
-  if ss -lnt "sport = :${HEALTH_VALUE}" | grep -q LISTEN; then
-    log "Port ${HEALTH_VALUE} is listening"
-    break
-  fi
+  log "Batch job detected. Skipping service start and health check."
 fi
