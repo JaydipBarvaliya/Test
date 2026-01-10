0a0607179db54048692613a75e14f803bb01b204
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-01-09 20:22:53 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,2 +1,19 @@
-public interface StorTxnRepository extends JpaRepository<StorTxnEntity, UUID> {
+@Entity
+@Table(name = "STOR_CONFIG")
+public class StorConfigEntity {
+
+    @Id
+    @Column(name = "LOB_ID", nullable = false, updatable = false)
+    private String lobId;
+
+    @Column(name = "STOR_SYS", nullable = false)
+    private String storageSystem;
+
+    @Column(name = "REPO_ID", nullable = false)
+    private String repoId;
+
+    @Column(name = "FOLDER_PATH", nullable = false)
+    private String folderPath;
+
+    // getters & setters
 }
