2b1a9965a031dfc6eb6a0773e9e96122522d832c
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-01-12 15:47:53 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,22 +1,3 @@
 package com.td.dgvlm.api.filenet.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import java.util.List;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public record BatchDocRequest(
-        String primaryRepositoryId,
-        List<ProcessBlock> process,
-        List<KeyValuePair> extractOption
-) {
-    public record ProcessBlock(
-            List<KeyValuePair> repositorySearchCriteria,
-            List<KeyValuePair> documentSearchCriteria,
-            List<KeyValuePair> option
-    ) {}
-
-    public record KeyValuePair(
-            String keyName,
-            Object keyValue
-    ) {}
-}
+public record BatchDocResponse(String batchId) {}
