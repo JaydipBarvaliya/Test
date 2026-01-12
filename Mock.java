628720dbb9ca21268be91cef0c5660ac1dbd35d9
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-01-12 15:47:07 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,3 +1,22 @@
-filenet.batchdoc.base-url=https://batchdoc-adms.api.dev.td.com
-filenet.batchdoc.extract-path=/v1/documents/batches/extracts
-filenet.batchdoc.timeout-seconds=30
+package com.td.dgvlm.api.filenet.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record BatchDocRequest(
+        String primaryRepositoryId,
+        List<ProcessBlock> process,
+        List<KeyValuePair> extractOption
+) {
+    public record ProcessBlock(
+            List<KeyValuePair> repositorySearchCriteria,
+            List<KeyValuePair> documentSearchCriteria,
+            List<KeyValuePair> option
+    ) {}
+
+    public record KeyValuePair(
+            String keyName,
+            Object keyValue
+    ) {}
+}
