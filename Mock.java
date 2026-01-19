48554c7cc4fac7930375e199f71452d0d6f83a0d
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-01-19 13:53:06 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,1 +1,22 @@
-2Nshv4tlCCjs5H@
+@Service
+public class TransactionStatusService {
+
+    private final StorTxnRepository repository;
+    private final TransactionMapper mapper;
+
+    public TransactionStatusService(
+            StorTxnRepository repository,
+            TransactionMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public TransactionResponse getTransactionStatus(String txnId) {
+        StorTxnEntity entity = repository.findById(txnId)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Transaction not found for txnId: " + txnId)
+                );
+
+        return mapper.toResponse(entity);
+    }
+}
