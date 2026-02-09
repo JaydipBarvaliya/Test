d70943d8c80cd75bb33cf9c8b25a5cd75970b7c8
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-02-09 13:20:00 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,11 +1,1 @@
-public List<ConfigurationDto> loadConfigurationFromDB(String appId) {
-    try {
-        return configurationService.findByAppId(List.of(appId, "shared"));
-    } catch (DataAccessException e) {
-        log.error("DB error while loading config for appId={}", appId, e);
-        throw e;
-    } catch (Exception e) {
-        log.error("Unexpected error while loading config for appId={}", appId, e);
-        throw new IllegalStateException("Failed to load configuration from DB", e);
-    }
-}
+mmPE6RN*9VnqMd
