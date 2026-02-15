0f059ae8115acd3f6faa79b2b640e958b242ad5a
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-02-15 16:57:13 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,3 +1,16 @@
 SELECT owner, object_type
 FROM all_objects
 WHERE object_name = 'STOR_INGEST_TXN';
+
+
+
+
+SELECT *
+FROM all_synonyms
+WHERE synonym_name = 'STOR_INGEST_TXN';
+
+
+
+SELECT text
+FROM all_views
+WHERE view_name = 'STOR_INGEST_TXN';
