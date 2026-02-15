f73bcbd36676e81c66ce5133f203c0287a8c5704
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-02-15 16:56:20 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,20 +1,3 @@
-@Service
-public class BatchDocAsyncWrapper {
-
-    private final BatchDocService batchDocService;
-
-    public BatchDocAsyncWrapper(BatchDocService batchDocService) {
-        this.batchDocService = batchDocService;
-    }
-
-    @Async
-    public void triggerAsync(
-            StorTransaction txn,
-            StorConfig storConfig,
-            String traceabilityId,
-            String primaryToken) {
-
-        batchDocService.triggerBatchDocAPI(
-                txn, storConfig, traceabilityId, primaryToken);
-    }
-}
+SELECT owner, object_type
+FROM all_objects
+WHERE object_name = 'STOR_INGEST_TXN';
