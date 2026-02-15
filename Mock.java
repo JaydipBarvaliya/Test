90540b435e6c371a3fde3aa6aeb78cae0c610ff2
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-02-15 17:02:26 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,16 +1,13 @@
-SELECT owner, object_type
-FROM all_objects
-WHERE object_name = 'STOR_INGEST_TXN';
-
-
-
-
-SELECT *
-FROM all_synonyms
-WHERE synonym_name = 'STOR_INGEST_TXN';
-
-
-
-SELECT text
-FROM all_views
-WHERE view_name = 'STOR_INGEST_TXN';
+@Query(value = """
+    SELECT ingest_txn_id
+    FROM (
+        SELECT ingest_txn_id
+        FROM stor_ingest_txn
+        WHERE status = 'ERROR'
+          AND state = 'RECEIVED'
+        FOR UPDATE SKIP LOCKED
+    )
+    WHERE ROWNUM <= :batchSize
+    """,
+    nativeQuery = true)
+List<String> lockErrorTransactions(@Param("batchSize") int batchSize);
