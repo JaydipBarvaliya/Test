2093bf3f1a4b5f554c92e0d21e2b22a519838a82
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-02-15 17:10:33 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,13 +1,21 @@
 @Query(value = """
     SELECT ingest_txn_id
-    FROM (
-        SELECT ingest_txn_id
-        FROM stor_ingest_txn
-        WHERE status = 'ERROR'
-          AND state = 'RECEIVED'
-        FOR UPDATE SKIP LOCKED
-    )
-    WHERE ROWNUM <= :batchSize
+    FROM stor_ingest_txn
+    WHERE status = 'ERROR'
+      AND state = 'RECEIVED'
+    FOR UPDATE SKIP LOCKED
     """,
     nativeQuery = true)
-List<String> lockErrorTransactions(@Param("batchSize") int batchSize);
+List<String> lockErrorTransactions();
+
+
+
+@Transactional
+public List<String> getBatch(int batchSize) {
+    return repo.lockErrorTransactions()
+               .stream()
+               .limit(batchSize)
+               .toList();
+}
+
+
