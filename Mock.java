772bf7fdf3047812e69fbfc86e712e0cf1ce87b6
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-02-27 17:12:04 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,28 +1,30 @@
-public void validateDrawerAndFolder(
-        IngestRequest ingestRequest,
-        String traceabilityId) {
+public void validateDrawerAndFolder(String lobId,
+                                    String traceabilityId,
+                                    IngestRequest ingestRequest)
+        throws ApiConfigException, ApiException, DgvLmServiceException {
 
     String drawerId = ingestRequest.getDigitalVault().getDrawerId();
     String folderId = ingestRequest.getDigitalVault().getFolderId();
-    String token = pingFedService.getOauth2ClientSecondaryToken();
+
+    log.debug("Validating drawer and folder. traceabilityId={}, lobId={}, drawerId={}, folderId={}",
+            traceabilityId, lobId, drawerId, folderId);
+
+    String pingfedToken = pingFedService.getOauth2ClientSecondaryToken();
 
     try {
+        webClientGateway.callDgvlaToValidateDrawerIdOrFolderId(
+                drawerId, folderId, lobId, traceabilityId, pingfedToken);
 
-        webClientGateway.callDgvlToValidateDrawerIdOrFolderId(
-                drawerId,
-                folderId,
-                traceabilityId,
-                token
-        );
+        log.debug("Drawer and folder validation successful. traceabilityId={}, drawerId={}, folderId={}",
+                traceabilityId, drawerId, folderId);
 
     } catch (Exception ex) {
 
-        throw new DgvlmServiceException(
-                new Status(
-                        String.valueOf(HttpStatus.BAD_REQUEST.value()),
-                        Severity.Error
-                ),
-                "Drawer or Folder validation failed in DG"
-        );
+        log.error("Drawer/Folder validation failed. traceabilityId={}, lobId={}, drawerId={}, folderId={}, error={}",
+                traceabilityId, lobId, drawerId, folderId, ex.getMessage(), ex);
+
+        throw new DgvLmServiceException(
+                new Status(String.valueOf(HttpStatus.BAD_REQUEST.value()), Severity.Error),
+                "Invalid drawerId or folderId.");
     }
 }
