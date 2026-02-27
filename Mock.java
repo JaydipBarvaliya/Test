86f88467b666a445246a52dc11cf4e510f212c89
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-02-27 13:28:31 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,96 +1,20 @@
-public ResponseEntity<Void> get(
-        String url,
-        Map<String, String> headers) {
-
-    return webClient
-            .get()
-            .uri(url)
-            .headers(httpHeaders -> headers.forEach(httpHeaders::add))
-            .retrieve()
-            .toBodilessEntity()
-            .block(Duration.ofSeconds(timeoutInSeconds));
-}
-
-
-
-public ResponseEntity<Void> validateFolder(
-        String drawerId,
-        String folderId,
-        String traceabilityId,
-        String primaryToken) {
-
-    String url = dgvlUrl + "/drawers/" + drawerId + "/folders/" + folderId;
-
-    return get(
-            url,
-            Map.of(
-                    ApiConstants.TRACEABILITY_ID, traceabilityId,
-                    HttpHeaders.AUTHORIZATION, ApiConstants.BEARER_PREFIX + primaryToken
-            )
-    );
-}
-
-
-
-private void validateDrawerAndFolder(
-        IngestRequest ingestRequest,
-        String traceabilityId,
-        String primaryToken) {
-
-    String drawerId = ingestRequest.getDigitalVault().getDrawerId();
-    String folderId = ingestRequest.getDigitalVault().getFolderId();
-
-    try {
-        ResponseEntity<Void> response =
-                webClientGateway.validateFolder(
-                        drawerId,
-                        folderId,
-                        traceabilityId,
-                        primaryToken
-                );
-
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            throw new DgvlmServiceException("Unexpected response from DigiVLA");
-        }
-
-    } catch (WebClientResponseException ex) {
-
-        if (ex.getStatusCode().value() == 404) {
-            throw new DgvlmServiceException(
-                    "Drawer or Folder not found in DigiVLA",
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-
-        throw new DgvlmServiceException(
-                "DigiVLA system unavailable",
-                HttpStatus.SERVICE_UNAVAILABLE
-        );
-    }
-}
-
-
-validateDrawerAndFolder(ingestRequest, traceabilityID, primaryToken);
-
-
 catch (WebClientResponseException.NotFound ex) {
     throw new DgvlmServiceException(
-            "Drawer or Folder not found in DigiVLA",
-            HttpStatus.BAD_REQUEST
+            new Status(String.valueOf(HttpStatus.BAD_REQUEST.value()), Severity.Error),
+            "Drawer or Folder not found in DG"
     );
 }
+
 catch (WebClientResponseException ex) {
     throw new DgvlmServiceException(
-            "DigiVLA returned error: " + ex.getStatusCode(),
-            HttpStatus.SERVICE_UNAVAILABLE
+            new Status(String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()), Severity.Error),
+            "DG returned error: " + ex.getStatusCode()
     );
 }
+
 catch (Exception ex) {
     throw new DgvlmServiceException(
-            "DigiVLA system unavailable",
-            HttpStatus.SERVICE_UNAVAILABLE
+            new Status(String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()), Severity.Error),
+            "DG system unavailable"
     );
 }
-
-
-
