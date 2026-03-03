41982cbdde544d38f0fba78e18b9809e8fd416d0
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-03-03 13:32:01 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,2 +1,2 @@
 sudo mount -t cifs //NSAPDVCS03.D2-TDBFG.COM/ICDMS_007F2 /mnt/dgvlm-nas \
--o username=TEDOCH351NASC,password=XXXX,domain=D2-TDBFG,vers=3.0
+-o username=TEDOCH351NASC,password="YOURPASSWORD",domain=D2-TDBFG,vers=3.0
