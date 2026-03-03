8ada97aaa90ff6183be2994500208f041c342e77
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-03-03 14:03:07 -0500

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,2 +1,5 @@
-sudo mount -t cifs //NSAPDVCS03.D2-TDBFG.COM/ICDMS_007F2 /mnt/dgvlm-nas \
--o username=TEDOCH351NASC,password="YOURPASSWORD",domain=D2-TDBFG,vers=3.0
+sudo mkdir -p /mnt/dgvlm-dna && \
+sudo chown springboot:springboot /mnt/dgvlm-dna && \
+sudo chmod 775 /mnt/dgvlm-dna && \
+sudo mount -t cifs //NSAPDVCS01.D2-TDBFG.COM/SRCHD_0027D /mnt/dgvlm-dna \
+-o username=TDGVLM942NASB,password="1k^\eCCeV;o5luF0ik94<9:Uk",domain=D2-TDBFG,vers=3.0
