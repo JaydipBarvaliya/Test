2de756f8297cfacf3374d1f016ab2403f36ec232
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-03-15 16:10:01 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,11 +1,18 @@
-@Component
-public class LogSimulator {
+<appender name="CONSOLE" class="ch.qos.logback.core.ConsoleAppender">
 
-    private static final Logger log =
-        LoggerFactory.getLogger("org.messaginghub.pooled.jms.JmsPoolSession");
+    <filter class="ch.qos.logback.core.filter.EvaluatorFilter">
+        <evaluator>
+            <expression>
+                logger.equals("org.messaginghub.pooled.jms.JmsPoolSession")
+                && message.contains("Ignoring exception while closing JMS Session")
+            </expression>
+        </evaluator>
+        <OnMatch>DENY</OnMatch>
+        <OnMismatch>NEUTRAL</OnMismatch>
+    </filter>
 
-    @PostConstruct
-    public void test() {
-        log.warn("Ignoring exception while closing JMS Session");
-    }
-}
+    <encoder>
+        <pattern>%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level [%thread] %logger - %msg%n</pattern>
+    </encoder>
+
+</appender>
