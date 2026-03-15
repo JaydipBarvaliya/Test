7ab4c0b09e810b3702ec5ab24513e9723f6d259a
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-03-15 15:53:49 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,5 +1,18 @@
-@Bean
-public ConnectionFactory connectionFactory() {
-    return new ActiveMQConnectionFactory("vm://embedded?broker.persistent=false");
-}
+@Configuration
+public class JmsConfig {
 
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory("vm://embedded?broker.persistent=false");
+    }
+
+    @Bean
+    public JmsPoolConnectionFactory pooledConnectionFactory(ConnectionFactory connectionFactory) {
+
+        JmsPoolConnectionFactory pool = new JmsPoolConnectionFactory();
+        pool.setConnectionFactory(connectionFactory);
+        pool.setMaxConnections(5);
+
+        return pool;
+    }
+}
