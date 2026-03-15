4bda8f94ddb1814647fcd0fbbc84a6b087f981f9
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-03-15 15:44:44 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,21 +1,4 @@
 @Bean
-public ConnectionFactory mqConnectionFactory() {
-    MQConnectionFactory factory = new MQConnectionFactory();
-    factory.setHostName("localhost");
-    factory.setPort(1414);
-    factory.setQueueManager("QM1");
-    factory.setChannel("DEV.APP.SVRCONN");
-    return factory;
-}
-
-
-
-@Bean
-public JmsPoolConnectionFactory pooledConnectionFactory(ConnectionFactory mqConnectionFactory) {
-
-    JmsPoolConnectionFactory pool = new JmsPoolConnectionFactory();
-    pool.setConnectionFactory(mqConnectionFactory);
-    pool.setMaxConnections(5);
-
-    return pool;
+public ConnectionFactory connectionFactory() {
+    return new ActiveMQConnectionFactory("vm://embedded?broker.persistent=false");
 }
