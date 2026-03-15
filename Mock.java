baac978df5c590b06c88f207c976459e9deffe8c
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-03-15 16:16:10 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,18 +1,10 @@
-<appender name="CONSOLE" class="ch.qos.logback.core.ConsoleAppender">
-
-    <filter class="ch.qos.logback.core.filter.EvaluatorFilter">
-        <evaluator>
-            <expression>
-                logger.equals("org.messaginghub.pooled.jms.JmsPoolSession")
-                && message.contains("Ignoring exception while closing JMS Session")
-            </expression>
-        </evaluator>
-        <OnMatch>DENY</OnMatch>
-        <OnMismatch>NEUTRAL</OnMismatch>
-    </filter>
-
-    <encoder>
-        <pattern>%d{yyyy-MM-dd HH:mm:ss.SSS} %-5level [%thread] %logger - %msg%n</pattern>
-    </encoder>
-
-</appender>
+<filter class="ch.qos.logback.core.filter.EvaluatorFilter">
+    <evaluator>
+        <expression>
+            return logger.equals("org.messaginghub.pooled.jms.JmsPoolSession")
+            &amp;&amp; message.contains("Ignoring exception while closing JMS Session");
+        </expression>
+    </evaluator>
+    <OnMatch>DENY</OnMatch>
+    <OnMismatch>NEUTRAL</OnMismatch>
+</filter>
