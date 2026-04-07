23d7f579e50729bfdc2d75afd0f3fe4afb4fdf63
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-04-07 14:39:51 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,5 +1,6 @@
-Host vmc2-main
-    HostName 10.53.148.86
+Host dgvlm-dev
+    HostName 10.53.148.25
     User TF5028
-    Port 22
     IdentityFile ~/.ssh/id_ed25519
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
