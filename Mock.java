98eba17966d526a9d1c4556b737747f1b13e259a
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-04-13 11:02:34 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,7 +1,1 @@
-tlog() {
-  tail -f "$@" | sed \
-    -e 's/ERROR/\o033[31m&\o033[0m/g' \
-    -e 's/WARN/\o033[33m&\o033[0m/g' \
-    -e 's/INFO/\o033[32m&\o033[0m/g' \
-    -e 's/Exception/\o033[35m&\o033[0m/g'
-}
+nhu#H83ENz&2mf
