24ff2eeb2fe54ae83ce711619bd7ef175f5d3a15
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-08-03 19:08:30 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,14 +1,12 @@
-p5cuOXRwLePMScQAvwvlH3EXFAUhePlVoph8XZckp3LHrm8Znipgcok4IxwUyrvn
-
-
-g3A7ZIXKl3LBYZJg5rvp82F7UMeguBWVWtW7VSwsrJScpV99B9jKF9wDjniijBXE
-
-
-
-
-
-25hVvIk5QiO6JVlQtOZNTuMf0t8c172TomQ4dVR8c8rmuBtSLOXZu2tJAVgxPMT9
-
-
-
-4Rgn8gZpXXy0qNUJx5j3hqrzLouCkpVCeJ6GV9hK0ZIvFx7KvQzpDSPEIvg24oqI
+2 BHK Mississauga
+base maintenance : 579
+special assessment fees 0
+property tax = 182
+Insurance 59
+Total = 820
+Monthly Mortgage = 2400 Mortgage 
+Condo actual price = 471900 
+what will be the interest rate and what will be the downpayment he may have paid?
+I am assuming interestrate is around 3.50 to 3.75 
+and downpayment is around 60K to 90K 
+but I want an accurate numbers. Can you find this out?
