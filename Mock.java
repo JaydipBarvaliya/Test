85d3ec6c4baab4270be3dc2de6b27280203156f5
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2026-08-23 12:21:50 -0400

    Update Mock.java

--- Mock.java
+++ Mock.java
@@ -1,12 +1,1 @@
-2 BHK Mississauga
-base maintenance : 579
-special assessment fees 0
-property tax = 182
-Insurance 59
-Total = 820
-Monthly Mortgage = 2400 Mortgage 
-Condo actual price = 471900 
-what will be the interest rate and what will be the downpayment he may have paid?
-I am assuming interestrate is around 3.50 to 3.75 
-and downpayment is around 60K to 90K 
-but I want an accurate numbers. Can you find this out?
+https://claude.ai/share/5992dcc4-4d78-47ad-8404-5c94b0d28f0b
